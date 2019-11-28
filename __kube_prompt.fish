5e781885e36fbc8a6e65fc70cb4bac5a16407cf1
Takuto Komazaki <takuto.komazaki@gree.net>
2019-11-28 07:25:14 +0000

    improve compatibility between GNU and BSD

--- __kube_prompt.fish
+++ __kube_prompt.fish
@@ -2,10 +2,13 @@
 # https://github.com/jonmosco/kube-ps1
 # https://github.com/Ladicle/fish-kubectl-prompt
 
+set -U cmd_test (which /usr/bin/test || which /bin/test)
+set -U stat_opts (man stat | grep BSD && echo -n "-f %m" || echo -n "-c %Y")
+
 function __kube_ps_update_cache
   function __kube_ps_cache_context
     set -l ctx (kubectl config current-context 2>/dev/null)
-    if /bin/test $status -eq 0
+    if $cmd_test $status -eq 0
       set -g __kube_ps_context "$ctx"
     else
       set -g __kube_ps_context "n/a"
@@ -14,7 +17,7 @@ function __kube_ps_update_cache
 
   function __kube_ps_cache_namespace
     set -l ns (kubectl config view --minify -o 'jsonpath={..namespace}' 2>/dev/null)
-    if /bin/test -n "$ns"
+    if $cmd_test -n "$ns"
       set -g __kube_ps_namespace "$ns"
     else
       set -g __kube_ps_namespace "default"
@@ -22,11 +25,11 @@ function __kube_ps_update_cache
   end
 
   set -l kubeconfig "$KUBECONFIG"
-  if /bin/test -z "$kubeconfig"
+  if $cmd_test -z "$kubeconfig"
     set kubeconfig "$HOME/.kube/config"
   end
 
-  if /bin/test "$kubeconfig" != "$__kube_ps_kubeconfig"
+  if $cmd_test "$kubeconfig" != "$__kube_ps_kubeconfig"
     __kube_ps_cache_context
     __kube_ps_cache_namespace
     set -g __kube_ps_kubeconfig "$kubeconfig"
@@ -35,8 +38,9 @@ function __kube_ps_update_cache
   end
 
   for conf in (string split ':' "$kubeconfig")
-    if /bin/test -r "$conf"
-      if /bin/test -z "$__kube_ps_timestamp"; or /bin/test (/usr/bin/stat -f '%m' "$conf") -gt "$__kube_ps_timestamp"
+    if $cmd_test -r "$conf"
+      if $cmd_test -z "$__kube_ps_timestamp"; or $cmd_test (stat $stat_opts "$conf") -gt "$__kube_ps_timestamp"
+#      if $cmd_test -z "$__kube_ps_timestamp"; or $cmd_test (stat -c '%Y' "$conf") -gt "$__kube_ps_timestamp"
         __kube_ps_cache_context
         __kube_ps_cache_namespace
         set -g __kube_ps_kubeconfig "$kubeconfig"
@@ -48,7 +52,7 @@ function __kube_ps_update_cache
 end
 
 function __kube_prompt
-  if /bin/test -z "$__kube_ps_enabled"; or /bin/test $__kube_ps_enabled -ne 1
+  if $cmd_test -z "$__kube_ps_enabled"; or $cmd_test $__kube_ps_enabled -ne 1
     return
   end
 
